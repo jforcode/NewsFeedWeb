015e5aa5db6261b57e7c25a84561f2f4635b7ff2
jforcode <jforcode@gmail.com>
2018-08-17 12:47:12 +0530

    fetching feed working

--- js/app.js
+++ js/app.js
@@ -2,11 +2,17 @@ import Vue from 'vue';
 import axios from 'axios';
 
 const apiUrl = 'http://localhost:8080';
+axios.defaults.headers.common['Content-Type'] = 'application/json; charset=utf-8';
 
 const app = new Vue({
 	el: '#feedApp',
 
 	data: {
+		consts: {
+			publisher: 'Publisher',
+			category: 'Category'
+		},
+
 		allPubsDialog: null,
 
 		categories: [],
@@ -16,65 +22,78 @@ const app = new Vue({
 		allPublishers: [],
 		mapSelectedPubs: {},
 
-		searchTerm: '',
-		appliedFilters: [],
+		sortBy: 'publishedOn',
+		descOrder: true,
 
-		countFeed: 0,
-		feed: []
+		searchTerm: '',
+		appliedFilters: {
+			categories: [],
+			publishers: []
+		},
+		currPageNum: 1,
+
+		countAllFeeds: 0,
+		feeds: []
 	},
 
 	methods: {
 		search: function() {
-			alert(this.searchTerm);
+			this.fetchFeed();
 		},
 
 		sort: function(sortBy, descOrder) {
-			alert(sortBy + ' ' + descOrder);
+			this.sortBy = sortBy;
+			this.descOrder = descOrder;
+
+			this.fetchFeed();
 		},
 
 		onCategorySelected: function(category) {
 			category.selected ?
-				this.addAppliedFilter('Category', category.value) :
-				this.removeAppliedFilter('Category', category.value);
+				this.addAppliedFilter(this.consts.category, category.value) :
+				this.removeAppliedFilter(this.consts.category, category.value);
 		},
 
 		onPublisherSelected: function(publisher) {
 			publisher.selected ?
-				this.addAppliedFilter('Publisher', this.getPublisherUiStr(publisher)) :
-				this.removeAppliedFilter('Publisher', this.getPublisherUiStr(publisher));
+				this.addAppliedFilter(this.consts.publisher, publisher.displayStr) :
+				this.removeAppliedFilter(this.consts.publisher, publisher.displayStr);
 		},
 
 		addAppliedFilter: function(type, value) {
-			this.appliedFilters.push({ type, value });
-			if (type === 'Publisher') {
+			if (type === this.consts.publisher) {
+				this.appliedFilters.publishers.push(value);
 				this.mapSelectedPubs[value] = true;
+
+			} else if (type === this.consts.category) {
+				this.appliedFilters.categories.push(value);
 			}
 		},
 
 		removeAppliedFilter: function(type, value) {
-			var ind = this.appliedFilters.findIndex(filter => filter.type === type && filter.value === value);
-			if (ind !== -1) {
-				this.appliedFilters.splice(ind, 1);
-				if (type === 'Publisher') {
+			if (type === this.consts.publisher) {
+				var ind = this.appliedFilters.publishers.findIndex(publisher => publisher === value);
+				if (ind !== -1) {
+					this.appliedFilters.publishers.splice(ind, 1);
 					delete this.mapSelectedPubs[value];
+				}
+
+			} else if (type === this.consts.category) {
+				var ind = this.appliedFilters.categories.findIndex(category => category === value);
+				if (ind !== -1) {
+					this.appliedFilters.categories.splice(ind, 1);
 				}
 			}
 		},
 
-		getPublisherUiStr: function({ publisher, publisherUrl }) {
-			return publisher + ' (' + publisherUrl + ')';
-		},
-
 		loadAllPublishers: function() {
-			axios
-				.get(apiUrl + '/publishers')
+			this.fetchPublishers()
 				.then(result => {
 					var i = 1;
 					this.countPublishers = result.data.countAllPublishers;
 					this.allPublishers = result.data.publishers.map(apiPublisher => {
 						var uiPublisher = getUiPublisher(apiPublisher, i++);
-						var uiPubDisplayStr = this.getPublisherUiStr(uiPublisher);
-						uiPublisher.selected = this.mapSelectedPubs[uiPubDisplayStr];
+						uiPublisher.selected = this.mapSelectedPubs[uiPublisher.displayStr];
 						return uiPublisher;
 					});
 
@@ -86,12 +105,12 @@ const app = new Vue({
 
 		selectPublishersFromDialog: function() {
 			this.allPublishers.forEach(publisher => {
-				var uiPubDisplayStr = this.getPublisherUiStr(publisher);
+				var uiPubDisplayStr = publisher.displayStr;
 				if (publisher.selected) {
 					if (!this.mapSelectedPubs[uiPubDisplayStr]) {
-						this.addAppliedFilter('Publisher', uiPubDisplayStr);
+						this.addAppliedFilter(this.consts.publisher, uiPubDisplayStr);
 					}
-					var mainUiPub = this.publishers.find(uiPublisher => this.getPublisherUiStr(uiPublisher) === uiPubDisplayStr);
+					var mainUiPub = this.publishers.find(uiPublisher => uiPublisher.displayStr === uiPubDisplayStr);
 					if (mainUiPub) {
 						mainUiPub.selected = true;
 					}
@@ -103,33 +122,72 @@ const app = new Vue({
 
 		closeAllPubsDialog: function() {
 			allPubsDialog.close();
+		},
+
+		fetchCategories: function() {
+			return axios.get(apiUrl + '/categories');
+		},
+
+		fetchPublishers: function(limit) {
+			return limit > 0
+				? axios.get(apiUrl + '/publishers', { params: { limit } })
+				: axios.get(apiUrl + '/publishers');
+		},
+
+		fetchFeed: function() {
+			var params = {
+				sort: {
+					sortBy: this.sortBy,
+					descOrder: this.descOrder
+				},
+				filter: {
+					categoryIds: this.appliedFilters.categories,
+					publishers: this.appliedFilters.publishers
+				},
+				page: this.currPageNum,
+				srch: this.searchTerm
+			};
+			console.log(params);
+			return axios.post(apiUrl + '/feed', params);
+		}
+	},
+
+	computed: {
+		isAnyFilterApplied: function () {
+			return this.appliedFilters.categories.length !== 0 
+				|| this.appliedFilters.publishers.length !== 0;
 		}
 	},
 
 	created: function() {
 		this.allPubsDialog = document.querySelector('#allPubsDialog');
 
-		axios
-			.get(apiUrl + '/categories')
+		this.fetchCategories()
 			.then(result => {
 				this.categories = result.data.map(getUiCategory);
 			});
 
-		axios
-			.get(apiUrl + '/publishers', { params: { limit: 5 } })
+		this.fetchPublishers(5)
 			.then(result => {
 				var i = 1;
 				this.countPublishers = result.data.countAllPublishers;
 				this.publishers = result.data.publishers.map(apiPublisher => getUiPublisher(apiPublisher, i++));
 			});
+
+		this.fetchFeed()
+			.then(result => {
+				this.countAllFeeds = result.data.countAllFeeds;
+				this.feeds = result.data.feeds.map(getUiFeed);
+			});
 	}
 });
+
 
 function getUiCategory(apiCategory) {
 	return {
 		value: apiCategory.value,
 		id: apiCategory.id,
-		selected: true
+		selected: false
 	};
 }
 
@@ -138,6 +196,13 @@ function getUiPublisher(apiPublisher, id) {
 		id,
 		publisher: apiPublisher.publisher,
 		publisherUrl: apiPublisher.publisherUrl,
-		selected: false
+		selected: false,
+		displayStr: apiPublisher.publisher + ' (' + apiPublisher.publisherUrl + ')'
 	};
+}
+
+function getUiFeed(apiFeed) {
+	var uiFeed = apiFeed;
+	uiFeed.publishedOnStr = new Date(apiFeed.publishedOn).toString();
+	return uiFeed;
 }
