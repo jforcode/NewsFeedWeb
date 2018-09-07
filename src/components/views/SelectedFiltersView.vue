2027af0ea1d1f97d68ab56642e316b99355852b9
jforcode <jforcode@gmail.com>
2018-09-07 18:14:33 +0530

    selected filters css for chips

--- src/components/views/SelectedFiltersView.vue
+++ src/components/views/SelectedFiltersView.vue
@@ -9,9 +9,7 @@
     <span class="applied-filter jb-chip"
       v-for="selectedFilter in appState.selectedFilters">
       <span class="jb-chip__text">{{ selectedFilter.label }}</span>
-      <button type="button" class="delete-filter-icon" @click="removeSelectedFilter(selectedFilter)">
-        <i class="material-icons">cancel</i>
-      </button>
+      <i class="material-icons delete-filter-icon" @click="removeSelectedFilter(selectedFilter)">cancel</i>
     </span>
   </div>
 </template>
@@ -37,6 +35,13 @@ export default {
 
 <style lang="css" scoped>
 .delete-filter-icon {
-  
+  cursor: pointer;
+}
+
+.selected-filter {
+  margin-left: 8px;
+}
+.applied-filter:first-child {
+  margin-left: 0;
 }
 </style>
